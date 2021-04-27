ec4edace237baa57c3687feecd49c191afa00745
Max <mrgasser@ucsc.edu>
2021-04-26 23:44:58 -0700

    adding first version of player movement along with some temp assets, also added notes doc

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+// Endless Runner Project
+
+// global variables
+let cursors;
+
+// Game configuration
 let config = {
     type: Phaser.CANVAS,
     width: 960,
     height: 540,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
     autoCenter: true,
     pixelArt: true,
     scene: [Menu, Play ]
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,9 +12,10 @@ class Play extends Phaser.Scene{
         this.load.image('parallax_2', './assets/parallax-mountain-mountains.png');
         this.load.image('parallax_3', './assets/parallax-mountain-trees.png');
         this.load.image('parallax_4', './assets/parallax-mountain-foreground-trees.png');
+        this.load.image('ground', "./assets/ground.png");
+        this.load.image('test_player', "./assets/test_player.png");
 
         // ------------------PLAYER
-
 
         // load a spritesheet
         // this.load.spritesheet('explosion', './assets/explosion.png', {
@@ -26,6 +27,15 @@ class Play extends Phaser.Scene{
     }
 
     create() {
+
+        // variables and settings
+        this.ACCELERATION = 1500;
+        this.MAX_X_VEL = 500;
+        this.MAX_Y_VEL = 5000; 
+        this.MAX_JUMPS = 2;
+        this.JUMP_VELOCITY = -700;
+        this.DRAG = 600;
+        this.physics.world.gravity.y = 2600;
 
         // Create and Place Parallax Background
         // ------------------------------------ Placing the base background
@@ -61,79 +71,25 @@ class Play extends Phaser.Scene{
         // Create Game Camera
         this.myCam = this.cameras.main;
 
-        // // making the camera follow the player
-        // this.myCam.startFollow(this.player);
+        // Ground
+        this.ground = this.physics.add.sprite(0, game.config.height - 70,
+        'ground').setOrigin(0, 0);
+        this.ground.body.immovable = true;
+        this.ground.body.allowGravity = false;
+        this.ground.setScrollFactor(0, 0);
 
-        // // green UI background
-        // this.add.rectangle(0, borderUISize + borderPadding, game.config.width,
-        //     borderUISize * 2, 0x00FF00).setOrigin(0,0);
-
-        // // white borders
-        // this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
-        // this.add.rectangle(0, game.config.height - borderUISize, game.config.width,
-        //     borderUISize, 0xFFFFFF).setOrigin(0,0);
-        // this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
-        // this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height,
-        //     0xFFFFFF).setOrigin(0,0);
-
-        // //add rocket (player 1)
-        // this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize -
-        //     borderPadding, 'rocket').setOrigin(0.5, 0);
-
-        // // add spaceship (x3)
-        // this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4,
-        //     'spaceship', 0, 30).setOrigin(0, 0);
-        // this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 +
-        //     borderPadding+2,'spaceship', 0, 20).setOrigin(0, 0);
-        // this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4,
-        //     'spaceship', 0, 10).setOrigin(0, 0);
-            
-
-        // // define keys for rocket p1
-        // keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        // keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-        // keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        // keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-
-        // // animation config
-        // this.anims.create({
-        //     key: 'explode',
-        //     frames: this.anims.generateFrameNumbers('explosion', {
-        //         start: 0,
-        //         end: 9,
-        //         first: 0
-        //     }),
-        //     frameRate: 30
-        // });
-
-        // // initialize score
-        // this.p1Score = 0;
-
-        // // display score
-        // let scoreConfig = {
-        //     fontFamily: 'Courier',
-        //     fontSize: '28px',
-        //     backgroundColor: '#F3B141',
-        //     color: '#843605',
-        //     align: 'right',
-        //     padding:{
-        //         top: 5,
-        //         bottom: 5,
-        //     },
-        //     fixedWidth: 100
-        // }
-        // this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+        // Test player
+        this.test_player = this.physics.add.sprite(game.config.width/2, game.config.height/2,
+        'test_player').setOrigin(0, 0);
+        this.test_player.setCollideWorldBounds(true);
+        this.test_player.setScrollFactor(0, 0);
+        //this.test_player.body.setScrollFactor(0, 0);
         
-        // // GAME OVER flag
-        // this.gameOver = false;
+        // setup cursor key input
+        cursors = this.input.keyboard.createCursorKeys(); 
 
-        // // 60-second play clock
-        // scoreConfig.fixedWidth = 0;
-        // this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
-        //     this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-        //     this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
-        //     this.gameOver = true;
-        // }, null, this);
+        // setup collider
+        this.physics.add.collider(this.test_player, this.ground);
     }
 
     createParallaxBackground(){
@@ -151,40 +107,42 @@ class Play extends Phaser.Scene{
         this.parallax_3.tilePositionX = this.myCam.scrollX * .5;
         this.parallax_4.tilePositionX = this.myCam.scrollX;
         
-        // // check key input for restart
-        // if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
-        //     this.scene.restart();
-        // }
+        // check keyboard input
+        //if(cursors.left.isDown) {}
+        if(cursors.left.isDown) {
+            this.test_player.body.setAccelerationX(-this.ACCELERATION);
+            this.test_player.setFlip(true, false);
+        } else if(cursors.right.isDown) {
+            this.test_player.body.setAccelerationX(this.ACCELERATION);
+            this.test_player.resetFlip();
+        } else {
+            // set acceleration to 0 so DRAG will take over
+            this.test_player.body.setAccelerationX(0);
+            this.test_player.body.setDragX(this.DRAG);
+        }
+    
+        // check if player is on the ground
+        this.test_player.isGrounded = this.test_player.body.touching.down;
 
-        // if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-        //     this.scene.start("menuScene");
-        // }
+        if(this.test_player.isGrounded) {
+            this.jumps = this.MAX_JUMPS;
+            this.jumping = false;
+        } else {
+            // play jumping animation
+        }
 
-        // // background movement
-        // this.starfield.tilePositionX -= starSpeed;
+        // allows for steady change up to a certain key down duration
+        if (this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(cursors.up, 150)) {
+            this.test_player.body.velocity.y = this.JUMP_VELOCITY;
+            this.jumping = true;
+        }
 
-        // if(!this.gameOver){
-        //     // update rocket
-        //     this.p1Rocket.update();
-        //     // update spaceships
-        //     this.ship01.update();
-        //     this.ship02.update();
-        //     this.ship03.update();
-        // }
-
-        // // check collisions 
-        // if(this.checkCollision(this.p1Rocket, this.ship03)){
-        //     this.p1Rocket.reset();
-        //     this.shipExplode(this.ship03);
-        // }
-        // if(this.checkCollision(this.p1Rocket, this.ship02)){
-        //     this.p1Rocket.reset();
-        //     this.shipExplode(this.ship02);
-        // }
-        // if(this.checkCollision(this.p1Rocket, this.ship01)){
-        //     this.p1Rocket.reset();
-        //     this.shipExplode(this.ship01);
-        // }
+        // letting go of the up key subtracts a jump
+        if (this.jumping && Phaser.Input.Keyboard.UpDuration(cursors.up)) {
+            this.jumps--;
+            this.jumping = false;
+        }
+       
     }
 
     checkCollision(obj1, obj2){
@@ -195,24 +153,5 @@ class Play extends Phaser.Scene{
             obj1.y + obj1.height > obj2.y){
                 return true;
         } else{ return false;} 
-    }
-
-    shipExplode(ship) {
-        // // temporarily hide ship
-        // ship.alpha = 0;
-        // // create explosion sprite at ship's position
-        // let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        // // play explode animation
-        // boom.anims.play('explode');
-        // // callback after anim completes
-        // boom.on('animationcomplete', () => {    
-        //     ship.reset();                         // reset ship position
-        //     ship.alpha = 1;                       // make ship visible again
-        //     boom.destroy();                       // remove explosion sprite
-        // });
-        // // score and repaint
-        // this.p1Score += ship.points;
-        // this.scoreLeft.text = this.p1Score;
-        // this.sound.play('sfx_explosion'); 
     }
 }
